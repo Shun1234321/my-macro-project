5b138276bfbf724a386a27f509162370f59f832a
Shun1234321 <206174051+ Shun1234321@ users.noreply.github.com>
2025-06-10 09:50:41 +0900

    国・期間の絞り込み

--- growth_accounting_assingnment2.py
+++ growth_accounting_assingnment2.py
@@ -4,3 +4,17 @@ import numpy as np
 # pwtの読み込み
 pwt90 = pd.read_stata('https://www.rug.nl/ggdc/docs/pwt90.dta')
 
+
+# 国の設定
+oecd_countries = [
+    "Australia", "Austria", "Belgium", "Canada", "Denmark", "Finland",
+    "France", "Germany", "Greece", "Iceland", "Ireland", "Italy", "Japan",
+    "Netherlands", "New Zealand", "Norway", "Portugal", "Spain", "Sweden",
+    "Switzerland", "United Kingdom", "United States"
+]
+
+# 対象の国/期間の絞り込み
+data = pwt90[
+    pwt90['country'].isin(oecd_countries) &
+    pwt90['year'].between(1990, 2019)
+]
