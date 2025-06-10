3a40945081c1a33fb75beb2519356c69e3c995fc
Shun1234321 <206174051+ Shun1234321@ users.noreply.github.com>
2025-06-10 12:52:02 +0900

    y=Ak^αについて対数を取る

--- growth_accounting_assingnment2.py
+++ growth_accounting_assingnment2.py
@@ -20,10 +20,10 @@ data = data[relevant_cols].dropna()
 
 # Calculate additional variables
 data['alpha'] = 1 - data['labsh']
-data['y_n'] = data['rgdpna'] / data['emp']  # Y/N
+data['ln_y'] = np.log(data['rgdpna'] / data['emp'])    # ln(Y/N)
 data['hours'] = data['emp'] * data['avh']  # L
-data['tfp_term'] = data['rtfpna'] ** (1 / (1 - data['alpha']))  # A^(1/(1-alpha))
-data['cap_term'] = (data['rkna'] / data['rgdpna']) ** (data['alpha'] / (1 - data['alpha']))  # (K/Y)^(alpha/(1-alpha))
+data['ln_k'] = np.log(data['rkna'] / data['hours'])             # ln(K/L)
+data['ln_a'] = data['ln_y'] - data['alpha'] * data['ln_k']      # ln(A)
 data['lab_term'] = data['hours'] / data['pop']  # L/N
 data = data.sort_values('year').groupby('countrycode').apply(lambda x: x.assign(
     alpha=1 - x['labsh'],
